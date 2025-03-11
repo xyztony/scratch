ded9474363383a82dbff34427ccaa019f4580932
xyztony <awshaw@pm.me>
2025-03-11 23:14:21 +0100

    add `concat-arrow-files`
    Modified-by: xyztony <awshaw@pm.me>

--- src/arrow_utils.clj
+++ src/arrow_utils.clj
@@ -3,6 +3,9 @@
    [tech.v3.libs.arrow :as arrow]
    [tech.v3.dataset :as ds]
    [tech.v3.resource :as resource]
+   [tick.core :as t]
+   [ham-fisted.lazy-noncaching :as hfnc]
+   [medley.core :as medley]
    [clojure.java.io :as io]
    [clojure.string :as str]))
 
@@ -11,7 +14,7 @@
   {:open-type :mmap
    :integer-datetime-types? true ; Use efficient integer representation for dates
    :text-as-strings? true ; Convert text to strings for easier handling
-   })
+   :key-fn keyword})
 
 (defn file-extension
   "Get the file extension to determine if it's an Arrow file"
@@ -32,7 +35,7 @@
    (let [opts (merge default-options options)]
      (arrow/stream->dataset path opts))))
 
-(def is-file? (fn [v] (.isFile v)))
+(defn is-file? [v] (.isFile v))
 
 (def ticker-yyyy-mm-dd__hh_mm_ss-patern-str
   "%s-(\\d{4}-\\d{2}-\\d{2}__\\d{2}_\\d{2}_\\d{2})\\.%s$")
@@ -43,6 +46,12 @@
       (format ticker ext)
       re-pattern))
 
+(defn ticker-yyyy-mm-dd__hh_mm_ss->date
+  [date-str]
+  (t/parse-date-time date-str (t/formatter "yyyy-MM-dd__HH_mm_ss")))
+
+
+
 (defn- file->pattern-matches [pattern file]
   (when-let [matches
              (and (is-file? file)
@@ -50,42 +59,58 @@
     {:file file
      :matches matches}))
 
+(def date-match-comp
+  (comparator
+   (fn [a b]
+     (t/<
+      (ticker-yyyy-mm-dd__hh_mm_ss->date (last a))
+      (ticker-yyyy-mm-dd__hh_mm_ss->date (last b))))))
+
+(defn concat-arrow-files
+  [{:keys [process-fn arrow] :as options} paths]
+  (let [opts (merge default-options arrow)]
+    (->> paths
+         (mapcat #(arrow/stream->dataset-seq % opts))
+         (hfnc/map process-fn)
+         (apply ds/concat))))
+
 (comment
-  (let [pattern
-        (ticker-yyyy-mm-dd__hh_mm_ss-pattern "btc" "arrow")
+  (let [pattern (ticker-yyyy-mm-dd__hh_mm_ss-pattern "btc" "arrow")
 
         files-with-matches
         (->> (file-seq (io/file "."))
-             (keep (partial file->pattern-matches pattern)))]
-    (resource/releasing!
-     ;; :todo
-     ))
+             (keep (partial file->pattern-matches pattern)))
+
+        process-fn (fn [ds] (ds/sort-by-column ds :time))]
+    (->> files-with-matches
+         (sort-by :matches date-match-comp)
+         (map (comp str :file))
+         (concat-arrow-files {:process-fn process-fn})))
 
   (require '[tech.v3.dataset.reductions :as ds-reduce])
   (require '[tech.v3.dataset.rolling :refer [rolling] :as ds-roll])
-  (require '[ham-fisted.lazy-noncaching :as nfln])
+  (require '[ham-fisted.lazy-noncaching :as hfnc])
   (require '[ham-fisted.lazy-caching :as cfln])
   (let [btc (arrow/stream->dataset-seq
              "./btc-2025-03-10__08_25_53.arrow"
              {:key-fn keyword})]
-    (-> (ds-reduce/aggregate
-         {:n-elems (ds-reduce/row-count)
-          :price-avg (ds-reduce/mean :px)
-          :price-sum (ds-reduce/sum :px)
-          :price-med (ds-reduce/prob-median :px)
-          :price-iqr (ds-reduce/prob-interquartile-range :px)
-          :n-dates (ds-reduce/count-distinct :tid :int32)}
-         btc)
-        clojure.pprint/pprint)
-    ;; (prn (first btc))
-    (nfln/map (fn [ds]
-                (-> (ds/sort-by-column ds :time <)
-                    (rolling {:window-type :fixed
-                              :column-name :time
-                              :window-size 10
-                              :relative-window-position :left}
-                             {:max (ds-roll/max :px)})
-                    clojure.pprint/pprint))
-                btc))
+    #_(-> (ds-reduce/aggregate
+           {:n-elems (ds-reduce/row-count)
+            :price-avg (ds-reduce/mean :px)
+            :price-sum (ds-reduce/sum :px)
+            :price-med (ds-reduce/prob-median :px)
+            :price-iqr (ds-reduce/prob-interquartile-range :px)
+            :n-dates (ds-reduce/count-distinct :tid :int32)}
+           btc)
+          clojure.pprint/pprint)
+    (-> (hfnc/map (fn [ds]
+                    (ds/sort-by-column ds :time <))
+                  btc)
+        first
+        (ds/sort-by-column :time)
+        ds/concat
+        clojure.pprint/pprint))
+
+  
   
   ,)
